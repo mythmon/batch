5b8fc1a878c22965897b05ecaaa9e5db3b9c9a33
Mike Cooper <mythmon@gmail.com>
2013-09-20 14:27:03 -0700

    Now you can lose.

--- media/js/batch.js
+++ media/js/batch.js
@@ -40,17 +40,36 @@ define('batch', ['canvas'], function() {
         size: 20
     };
 
+    var grid = [];
+    var i, x, y;
+    var gWidth = width /gridSize;
+    var gHeight = height / gridSize;
+    for (x = 0; x < gWidth; x++) {
+        grid[x] = [];
+        for (y = 0; y < gHeight; y++) {
+            grid[x][y] = 0;
+        }
+    }
+
+    for (i = 0; i < 20; i++) {
+        x = Math.floor(Math.random() * gWidth);
+        y = Math.floor(Math.random() * gHeight);
+        grid[x][y] = 1;
+    }
+    grid[gWidth - 1][0] = 2;
+
     var animations = [];
     var program = [];
     var running = false;
     var currentInstruction = null;
 
     // program instructions
+    var animFrames = 5;
     var instructions = {
-        up: simpleAnimInstruction(10, 0, -gridSize / 10),
-        down: simpleAnimInstruction(10, 0, gridSize / 10),
-        left: simpleAnimInstruction(10, -gridSize / 10, 0),
-        right: simpleAnimInstruction(10, gridSize / 10, 0),
+        up: simpleAnimInstruction(animFrames, 0, -gridSize / animFrames),
+        down: simpleAnimInstruction(animFrames, 0, gridSize / animFrames),
+        left: simpleAnimInstruction(animFrames, -gridSize / animFrames, 0),
+        right: simpleAnimInstruction(animFrames, gridSize / animFrames, 0),
     }
 
     // scaffold
@@ -65,7 +84,6 @@ define('batch', ['canvas'], function() {
     }
 
     function run() {
-        console.log('running');
         running = true;
     }
 
@@ -83,11 +101,31 @@ define('batch', ['canvas'], function() {
             running = false;
             currentInstruction = null;
             updateProgramDisplay();
-            return;
+        } else {
+            currentInstruction = instruction;
+            instruction.func();
+            updateProgramDisplay();
         }
-        currentInstruction = instruction;
-        instruction.func();
-        updateProgramDisplay();
+        if (currentInstruction) {
+            if (currentInstruction.after) {
+                currentInstruction.after();
+            }
+        }
+        afterEveryInstruction();
+    }
+
+    function afterEveryInstruction() {
+        var gx = Math.floor(robot.x / gridSize);
+        var gy = Math.floor(robot.y / gridSize);
+
+        if (!currentInstruction && grid[gx][gy] == 2) {
+            alert('winner!');
+            running = false;
+        }
+        if (grid[gx][gy] == 1) {
+            alert('loser');
+            running = false;
+        }
     }
 
     function updateProgramDisplay() {
@@ -112,7 +150,7 @@ define('batch', ['canvas'], function() {
     }
 
     function tick() {
-        var i;
+        var i, x, y;
 
         ctx.fillStyle = '#000';
         ctx.fillRect(0, 0, width, height);
@@ -127,6 +165,21 @@ define('batch', ['canvas'], function() {
             ctx.stroke();
         }
 
+        ctx.fillStyle = '#0f0';
+        ctx.fillRect(width - gridSize, 0, gridSize, gridSize);
+
+        colors = ['#000', '#f00', '#0f0'];
+        for (x = 0; x < gWidth; x++) {
+            for (y = 0; y < gWidth; y++) {
+                ctx.fillStyle = colors[grid[x][y]];
+                ctx.fillRect(x * gridSize + 1, y * gridSize + 1,
+                             gridSize - 1, gridSize - 1);
+            }
+        }
+
+        ctx.fillStyle = '#00f';
+        ctx.fillRect(robot.x, robot.y, robot.size, robot.size);
+
         if (running) {
             if (animations.length) {
                 var anim = animations[0];
@@ -139,9 +192,6 @@ define('batch', ['canvas'], function() {
                 nextInstruction();
             }
         }
-
-        ctx.fillStyle = '#00f';
-        ctx.fillRect(robot.x, robot.y, robot.size, robot.size);
 
         setTimeout(tick, 30);
     }
